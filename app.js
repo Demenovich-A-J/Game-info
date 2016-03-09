adf7e3a202795dc46c33dac72b32cac86271eb03
Metiska <demenoff.aj@gmail.com>
2016-03-09 20:36:43 +0300

    Express+Package.json

--- app.js
+++
@@ -1,9 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandler");
-
-var handler = {};
-handler["/getAllGames"] = requestHandlers.LoadAllGamesInfo;
-handler["/getGameInfo"] = requestHandlers.LoadGameInfoById;
-
-server.start(router.route, handler);
---
+++ src/router.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var gameLoader = require('./GameLoader');
+
+var router = express.Router();
+var GameLoader = new gameLoader.GamesInfoLoader();
+
+router.get('/AllGames', function(request, response){
+	GameLoader.LoadAllGamesInfo(function(params){
+		response.json(JSON.parse(params));
+	});
+});
+
+router.get('/GameInfo', function(request, response){
+	GameLoader.LoadGameInfoById(request.query.appid, function(params){
+		response.json(JSON.parse(params));
+	});
+});
+
+module.exports = router;
