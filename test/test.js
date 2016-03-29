214bca67e35abf54acacc672149e092251ad8b4a
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2016-03-29 20:26:23 +0300

    jscs eslint

--- test/test.js
+++ test/test.js
@@ -1,35 +1,33 @@
-var request = require('supertest');
 var nock = require('nock');
-var testHelper = require('../helpers/testHelper');
 var should = require('chai').should();
 
 var gameLoader = require('../src/GameLoader');
 var GameLoader = new gameLoader.GamesInfoLoader();
 
 describe('Games tests', function(){
-    describe("Load all games info", function(){
-        it("should return json info about all games", function(done){
-            nock("http://api.steampowered.com")
-                .get("/ISteamApps/GetAppList/v2")
-                .reply(200, "Valid all games info.");
+    describe('Load all games info', function(){
+        it('should return json info about all games', function(done){
+                nock('http://api.steampowered.com')
+                .get('/ISteamApps/GetAppList/v2')
+                .reply(200, 'Valid all games info.');
 
         GameLoader.LoadAllGamesInfo(function(body){
-            body.should.equal("Valid all games info.");
+            body.should.equal('Valid all games info.');
             done();
         });
      });
  });
 
-  describe("Load info about game with id = 570", function(){
-     it("should return json data about game with id = 570", function(done){
-         nock("http://store.steampowered.com")
-             .get("/api/appdetails?appids=570")
-             .reply(200, "Valid game with id = 570 info");
+  describe('Load info about game with id = 570', function(){
+     it('should return json data about game with id = 570', function(done){
+         nock('http://store.steampowered.com')
+             .get('/api/appdetails?appids=570')
+             .reply(200, 'Valid game with id = 570 info');
 
          GameLoader.LoadGameInfoById(570, function(body){
-             body.should.equal("Valid game with id = 570 info");
+             body.should.equal('Valid game with id = 570 info');
              done();
          });
-     })
+     });
   });
 });
